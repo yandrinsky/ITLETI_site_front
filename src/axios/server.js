df9e7e11322e80cbfc33f22772a3fbccd0db59f3
vlad <you@example.com>
2021-10-19 01:12:14 +0300

    server link fix

--- src/axios/server.js
+++ src/axios/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:5000/"
-// export const server = "https://itleti-back.herokuapp.com/"
+// export const server = "http://localhost:5000/"
+export const server = "https://itleti-back.herokuapp.com/"
 
